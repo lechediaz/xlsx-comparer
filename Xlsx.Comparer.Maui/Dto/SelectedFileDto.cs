054ea7d8a0bd67c5e5ec0899774334dccd09c9ce
Oscar David Díaz <lechediaz@gmail.com>
2024-10-31 04:35:07 -0500

    Se permite comparar archivos XLSX
    - Se utiliza FilePicker para seleccionar archivo del dispositivo.
    - Se usa paquete ClosedXML para trabajar con archivos XLSX.
    - Se utiliza FileSystem.AppDataDirectory para obtener la ruta donde guardar archivos de la aplicación.
    - Se utiliza Share para compartir archivo generado.

---
+++ Xlsx.Comparer.Maui/Dto/SelectedFileDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xlsx.Comparer.Maui.Dto
+{
+    /// <summary>
+    /// Define la estructura de un archivo seleccionado
+    /// </summary>
+    public record SelectedFileDto(bool IsSelected, string FullPath)
+    {
+
+    }
+}
